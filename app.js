291a389502c71168b307d78a78ac9fe99832dbde
Tomoro0726 <tomoro0726@gmail.com>
2022-04-26 22:06:33 +0900

    sqlうまくいきません。

--- app.js
+++ app.js
@@ -1,24 +1,51 @@
-const express=require('express');
-const app=express();
-app.use(express.static('public'));
+const express = require("express");
+const app = express();
+app.use(express.static("public"));
+const mysql = require("mysql");
 
-app.get('/',(req,res) => {
-    res.render('index.ejs');
-});
-app.get('/competition',(req,res)=>{
-    res.render('competition.ejs')
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
+
+const connection = mysql.createConnection({
+  host: "127.0.0.1",
+  user: "root",
+  password: "4460726tomoro",
+  database: "sports",
 });
 
-app.get('/new',(req,res)=>{
-    res.render('new.ejs')
+app.get("/", (req, res) => {
+  res.render("index.ejs");
 });
 
-app.get('/edit',(req,res)=>{
-    res.render('edit.ejs')
+app.get("/competition", (req, res) => {
+  res.render("competition.ejs");
 });
 
-app.get('/all',(req,res)=>{
-    res.render('all.ejs')
+app.get("/new", (req, res) => {
+  res.render("new.ejs");
+});
+
+app.post("/newcompetition", (req, res) => {
+  console.log(req.body);
+  connection.query(
+    'insert into table1(name) values(?);',
+    [req.body.competition_name],
+    (errow, result) => {
+      console.log(errow);
+      res.redirect('/competition');
+    }
+  );
+});
+
+app.get("/edit", (req, res) => {
+  res.render("edit.ejs");
+});
+
+app.get("/all", (req, res) => {
+  res.render("all.ejs");
 });
 
 app.listen(3000);
