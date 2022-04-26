08cfbf8695c9b9a283539fd645e1f36ca17b2e0f
Tomoro <tomoro0726@gmail.com>
2022-04-26 14:16:02 +0900

    がんばろう日本

--- app.js
+++ app.js
@@ -13,4 +13,12 @@ app.get('/new',(req,res)=>{
     res.render('new.ejs')
 });
 
+app.get('/edit',(req,res)=>{
+    res.render('edit.ejs')
+});
+
+app.get('/all',(req,res)=>{
+    res.render('all.ejs')
+});
+
 app.listen(3000);
