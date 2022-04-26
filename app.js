0d0788cdbb3d5ac5e32002166a8a64651fa95946
Tomoro <tomoro0726@gmail.com>
2022-04-26 11:42:32 +0900

    得点登録

--- app.js
+++ app.js
@@ -2,12 +2,11 @@ const express=require('express');
 const app=express();
 app.use(express.static('public'));
 
-
-
-
 app.get('/',(req,res) => {
     res.render('index.ejs');
 });
-
+app.get('/competition',(req,res)=>{
+    res.render('competition.ejs')
+});
 
 app.listen(3000);
