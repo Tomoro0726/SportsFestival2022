65b8d3ccbbc05dc4186596b5d60b3553aa5fc17d
Tomoro0726 <tomoro0726@gmail.com>
2022-05-25 21:14:07 +0900

    降順及び昇順追加完了

--- app.js
+++ app.js
@@ -18,6 +18,29 @@ const connection = mysql.createConnection({
 
 app.get("/", (req, res) => {
   res.render("index.ejs")
-})
+});
+app.get("/register", (req, res) => {
+  res.render("register.ejs")
+});
+app.get("/registercreate", (req, res) => {
+  res.render("registercreate.ejs")
+});
+app.post("/registercreate", (req, res) => {
+  if (req.body.hyouka == "昇順") {
+    var hyouka = true;
+  } else if (req.body.hyouka == "降順") {
+    var hyouka = false;
+  };
+  console.log(hyouka);
+  connection.query(
+    'INSERT INTO tokuten (name,grade,one,two,three,four,five,six,seven,eight,hyouka) VALUES (?,?,?,?,?,?,?,?,?,?,?);',
+    [req.body.name, req.body.grade, req.body.one, req.body.two, req.body.three, req.body.four, req.body.five, req.body.six, req.body.seven, req.body.eight, hyouka],
+    (errow, result) => {
+      console.log(errow);
+      console.log(req.body);
+      res.redirect('/register');
+    }
+  );
+});
 
 app.listen(3000);
